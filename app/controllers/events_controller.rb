642ffd07fbbe709067247d2548f037eeb4536d5e
Michael Daryl Mayo <michaeldaryl.mayo@protonmail.com>
2017-10-11 21:01:02 +0800

    Added routes, controllers, and view files

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,28 @@
+class EventsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def join
+  end
+
+  def cancel
+  end
+
+  def edit
+  end
+
+  def location
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def event
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def update
+  end
+end
