720151d895bb98fcb04eb0aa0ddd83f4fe2d1632
Michael Daryl Mayo <michaeldaryl.mayo@protonmail.com>
2017-10-11 20:54:28 +0800

    Added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,58 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => "events#index"
+
+  # USERS routes
+  get 'users/new' => 'users#new'
+
+  get 'users/:id/edit' => 'users#edit'
+
+  get 'users/:id/events' => 'users#event'
+
+  post 'users' => 'users#create'
+
+  get 'users/destroy'
+
+  get 'users/:id'=> 'users#show'
+
+  put 'users/:id' => 'users#update'
+
+  #SESSIONS routes
+
+  post 'sessions' => 'sessions#create'
+
+  get 'sessions/new' => 'sessions#new'
+
+  get 'sessions/create'
+
+  delete 'sessions'=> 'sessions#destroy'
+
+  # EVENTS routes
+
+  get 'events' => 'events#index'
+
+  post 'events' => 'events#create'
+
+  post 'events/join' => 'events#join'
+
+  post 'events/cancel' => 'events#cancel'
+
+  get 'events/:id/edit' => 'events#edit'
+
+  get 'events/location' => 'events#location'
+
+
+  put 'events/:id' => 'events#update'
+
+  delete 'events/:id' =>  'events#destroy'
+
+  get 'events/:id' => 'events#show'
+
+
+
+  #COMMENTS routes
+
+  post 'comments' => 'comments#create'
+
+  get 'comments' => 'comments#index'
 end
