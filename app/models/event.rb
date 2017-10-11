6eaa4839eb61632b71800242d6c90a1329f8820b
Michael Daryl Mayo <michaeldaryl.mayo@protonmail.com>
2017-10-11 19:33:09 +0800

    Added and configured models

---
+++ app/models/event.rb
@@ -0,0 +1,9 @@
+class Event < ApplicationRecord
+  belongs_to :user, foreign_key: "host"
+  has_many :user_events, dependent: :destroy
+  has_many :users, through: :user_events
+  has_many :comments, dependent: :destroy
+
+  validates :name, :date, :location, :state, :presence => true
+  validates :date, date: { after: Proc.new { Date.today }, message: 'must be after  today' }, on: :create
+end
